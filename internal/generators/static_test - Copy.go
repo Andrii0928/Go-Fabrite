42cacacbae0a1ae3b8eb87a62115ae51fdee0218
Artem1280 <katli90210@gmail.com>
2022-03-14 11:29:58 -0400

    Update Version

---
+++ internal/generators/static_test - Copy.go
@@ -0,0 +1,34 @@
+package generators
+
+import (
+	"testing"
+
+	"github.com/microsoft/fabrikate/internal/core"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStaticGenerator_Generate(t *testing.T) {
+	component := core.Component{
+		Name:         "foo",
+		Path:         "",
+		PhysicalPath: "../../testdata/invaliddir",
+	}
+
+	generator := &StaticGenerator{}
+	_, err := generator.Generate(&component)
+	assert.NotNil(t, err)
+}
+
+func TestGetStaticComponentPath(t *testing.T) {
+	component := core.Component{
+		Name:          "kv-flexvol",
+		ComponentType: "static",
+		Method:        "http",
+		Source:        "https://raw.githubusercontent.com/Azure/kubernetes-keyvault-flexvol/master/deployment/kv-flexvol-installer.yaml",
+	}
+
+	expectedComponentPath := "components/kv-flexvol"
+	componentPath := GetStaticManifestsPath(component)
+
+	assert.Equal(t, expectedComponentPath, componentPath)
+}
